3c2bbfbdf6e72a7d952a2111f2a7bc070274fce8
Xin Luigi Chen <luxchen@ucdavis.edu>
2018-11-15 19:49:37 -0800

    Simple implementation of feedforward neural net

---
+++ neural_net.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+#sigmoid
+def nonlinear(x, deriv = False):
+    if(deriv == True):
+        return (x * (1 - x))
+
+    return 1 / (1 + np.exp(-x))
+
+#input data
+x = np.array([[0, 0, 1], [0, 1, 1], [1, 0, 1], [1, 1, 1]])
+
+y = np.array([[0], [1], [1], [0]])
+
+#seed
+np.random.seed(1)
+
+#synapses
+syn0 = 2 * np.random.random((3, 4)) - 1
+syn1 = 2 * np.random.random((4, 1)) - 1
+print syn0
+print syn1
+
+#training
+for i in xrange(60000):
+    #layers
+    l0 = x
+    l1 = nonlinear(np.dot(l0, syn0))
+    l2 = nonlinear(np.dot(l1, syn1))
+
+    #backpropagation
+    l2_error = y - l2
+    if(i % 10000) == 0:
+        print "Error: " + str(np.mean(np.abs(l2_error)))
+
+    #calculate deltas
+    l2_delta = l2_error * nonlinear(l2, deriv = True)
+    l1_error = l2_delta.dot(syn1.T)
+    l1_delta = l1_error * nonlinear(l1, deriv = True)
+
+    #update synapses
+    syn1 += l1.T.dot(l2_delta)
+    syn0 += l0.T.dot(l1_delta)
+
+print "Output after training: "
+print l2
